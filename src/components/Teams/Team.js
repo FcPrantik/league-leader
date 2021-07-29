e3973885e83129e2987b991a8632500f97b80df7
Farshad Chowdhury <prantik@Farshads-MacBook-Pro.local>
2021-07-29 15:58:02 +0600

    responsive

--- src/components/Teams/Team.js
+++ src/components/Teams/Team.js
@@ -5,11 +5,13 @@ const Team = (props) => {
     const { strTeam, strSport, idTeam, strTeamBadge,} = props.team;
     
     return (
-        <div className = "badge">
+        <div className = "container">
+            <div className = "badge">
             <img src= {strTeamBadge} alt="" />
             <h2>{strTeam}</h2>
             <p>Sports type: {strSport}</p>
             <Link to={`/${idTeam}`}><button className= "btn btn-primary">Explore</button></Link>
+            </div>
         </div>
     );
 };
