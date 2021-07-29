1e94e652ca30e58e727e958d4ebadb7deffaa312
Farshad Chowdhury <prantik@Farshads-MacBook-Pro.local>
2021-07-29 14:00:11 +0600

    LinkRemoved

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import Home from './components/Home/Home';
 import NoMatch from './components/NoMatch/NoMatch';
